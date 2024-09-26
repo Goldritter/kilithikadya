47c9454d7c4e1527cc67b9691afd6d89a6280823
Goldritter <marcus.goldritter.lindner@gmail.com>
2024-09-26 16:33:35 -0400

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kilithikadya
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject kilithikadya "0.1.0-SNAPSHOT"
+  :description "Probability calculation application for Warhammer 40k 10'th edition."
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.apache.commons/commons-statistics-descriptive "1.1"]
+                 [org.apache.commons/commons-statistics-distribution "1.1"]
+                 [org.apache.commons/commons-statistics-inference "1.1"]
+                 [org.clojure/math.combinatorics "0.3.0"]
+                 ]
+  :main ^:skip-aot kilithikadya.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
