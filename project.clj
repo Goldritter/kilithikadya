0ae8f815f0229ac8b17ecb717fd1d26950e5e218
Goldritter <marcus.goldritter.lindner@gmail.com>
2025-01-02 18:25:15 +0100

    License

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject com.github.goldritter/kilithikadya "0.1.1-a.5"
   :description "Probability calculation application for Warhammer 40k 10'th edition."
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "https://choosealicense.com/licenses/mit"
+            :comment "MIT License"
+            :year 2025
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.apache.commons/commons-statistics-descriptive "1.1"]
                  [org.apache.commons/commons-statistics-distribution "1.1"]
